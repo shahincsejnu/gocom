3b9009e018c9908241f6105d500c7dd4389dc62c
Sahadat Hossain <shahincsejnu@gmail.com>
2023-02-04 15:53:25 +0600

    Add HealthCheck endpoint in ecom service
    Signed-off-by: Sahadat Hossain <shahincsejnu@gmail.com>

--- ecom/main.go
+++ ecom/main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/shahincsejnu/gocom/ecom/presenter/healthcheck"
 )
 
 func main() {
-	fmt.Println("Hello from ecom!")
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	server, err := newServer()
+	if err != nil {
+		return err
+	}
+
+	log.Println("server is running...")
+	return server.ListenAndServe()
+}
+
+func newServer() (*http.Server, error) {
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.Default()
+
+	r.GET("/", healthcheck.Handler())
+
+	return &http.Server{Addr: ":8080", Handler: r}, nil
 }
